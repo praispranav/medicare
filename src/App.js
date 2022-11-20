003618d1981cc9ec01c41c98b5a34c0bf5081e5d
Pranav <praispranav@gmail.com>
2022-11-21 02:32:38 +0530

    Added Visitor and Remove unnecessary script

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect } from 'react';
 import './App.css';
 import { AppStart } from './modules/AppStart/AppStart';
-
+import { localStorageKeys } from "./constants/localStorage.ts";
+import { v4 as uuid } from 'uuid';
 
 function App() {
+
+  const checkOrCreateVisitorId = () => {
+    const localStorageVisitorId = localStorage.getItem(
+      localStorageKeys.visitorId
+    );
+    if (localStorageVisitorId) {
+    } else {
+      const newVisitorId = uuid();
+      localStorage.setItem(localStorageKeys.visitorId, newVisitorId);
+    }
+  };
+
+  useEffect(()=>{
+    checkOrCreateVisitorId();
+  },[])
+
   return (
     <div className="App">
       <AppStart />
