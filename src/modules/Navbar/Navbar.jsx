94be7d74b9b850bb35a3f6f5db272857934baf0b
nikhil rajpal <nikhil@wecallmedia.com>
2022-11-24 05:02:03 +0530

    logo fixed

--- src/modules/Navbar/Navbar.jsx
+++ src/modules/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Navbar.css';
+import './Navbar.scss';
 
 import logo from '../../assets/logo.png';
 import phoneicon from '../../assets/phone.svg';
@@ -9,18 +9,23 @@ export const Navbar = () => {
     return <div className="navbar">
         <nav>
             <div className='logoholder'>
-                <img src={logo} alt="logo" />
+                <div className='logo'>
+                    <img src={logo} alt="logo" />
+                    <div className='sitename'>
+                        <h1>QualifyQuotes.us</h1>
+                        <h2>a non-government medicare site</h2>
+                    </div>
+                </div>
             </div>
             <a href="tel:18008148552">
                 <div className="phone-icon">
                     <img src={phoneicon} alt="logo" />
                 </div>
                 <div class="flex flex-col items-center phone-number-holder">
-                    <div class="text-phone-number-holder">CALL TOLL-FREE M-F 7AM-10PM CST</div>
-                    <div class="primary-text-dark flex items-center">
-                        <div class="bg-green-400 rounded-full w-2 h-2 mr-2.5 lg:hidden"></div>
-                        <div class="absolute bg-green-400 animate-ping rounded-full w-2 h-2 mr-2.5 lg:hidden"></div>
-                        <div class="flex space-x-1 items-center text-xl whitespace-nowrap">
+                    <div class="text-phone-number">CALL TOLL-FREE M-F 7AM-10PM CST</div>
+                    <div class="toll-free-number-holder">
+                        <div class="available-blinker"></div>
+                        <div class="toll-free-number">
                             <span class="font-bold tracking-tighter">1-800-814-8559</span>
                             <span class="font-semibold">(TTY 711)</span>
                         </div>
