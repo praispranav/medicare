b3ecae38784f51a028d83155f091dcba8f0e8761
nikhil rajpal <nikhil@wecallmedia.com>
2022-11-25 23:27:43 +0530

    updated-url-params

--- src/constants/queryStrings.js
+++ src/constants/queryStrings.js
@@ -1,18 +1,8 @@
 export const REDIRECT_AND_STORAGE_KEYS = [
     {
-        redirectString:"CID",
-        storageKey: "Campaign_Name",
-        ringbaKey:"cid"
-    },
-    {
-        redirectString:"ADS_ID",
-        storageKey: "Adset_Name",
-        ringbaKey:"ads_id"
-    },
-    {
-        redirectString:"ADID",
-        storageKey: "Ad_Name",
-        ringbaKey:"adid"
+        redirectString:"device",
+        storageKey: "device",
+        ringbaKey:"device"
     },
     {
         redirectString:"fbclid",
@@ -20,9 +10,14 @@ export const REDIRECT_AND_STORAGE_KEYS = [
         ringbaKey:"fbclid"
     },
     {
-        redirectString:"utm_source",
-        storageKey: "utm_source",
-        ringbaKey:"utm_source"
+        redirectString:"gclid",
+        storageKey: "gclid",
+        ringbaKey:"gclid"
+    },
+    {
+        redirectString:"utm_aceid",
+        storageKey: "utm_aceid",
+        ringbaKey:"utm_aceid"
     },
     {
         redirectString:"generator",
@@ -34,5 +29,44 @@ export const REDIRECT_AND_STORAGE_KEYS = [
         storageKey: "interest",
         ringbaKey:"interest"
     },
-
+    {
+        redirectString:"utm_adset",
+        storageKey: "utm_adset",
+        ringbaKey:"utm_adset"
+    },
+    {
+        redirectString:"utm_campaign",
+        storageKey: "utm_campaign",
+        ringbaKey:"utm_campaign"
+    },
+    {
+        redirectString:"utm_content",
+        storageKey: "utm_content",
+        ringbaKey:"utm_content"
+    },
+    {
+        redirectString:"utm_id",
+        storageKey: "utm_id",
+        ringbaKey:"utm_id"
+    },
+    {
+        redirectString:"utm_medium",
+        storageKey: "utm_medium",
+        ringbaKey:"utm_medium"
+    },
+    {
+        redirectString:"utm_placement",
+        storageKey: "utm_placement",
+        ringbaKey:"utm_placement"
+    },
+    {
+        redirectString:"utm_source",
+        storageKey: "utm_source",
+        ringbaKey:"utm_source"
+    },
+    {
+        redirectString:"utm_term",
+        storageKey: "utm_term",
+        ringbaKey:"utm_term"
+    },
 ]
