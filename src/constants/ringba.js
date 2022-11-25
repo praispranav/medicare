41fd5443e4fd2e47e3d05acf7471273e77386287
nikhil rajpal <nikhil@wecallmedia.com>
2022-11-26 02:44:50 +0530

    main-tfn

--- src/constants/ringba.js
+++ src/constants/ringba.js
@@ -4,16 +4,16 @@ export const GENERATOR_QUERY = "generator";
 export const RINGBA_SCRIPT_ID = "rgba_script"
 
 const RINGBA_COM_TAGS = [
-  // {
-  //   key: "JSbfbe6e3aef084885af8a574bec4f8d45",
-  //   user: "internal1", // make default
-  //   number: "1-866-790-0241",
-  // },
   {
-    key: 'JS8d271f1ea8034bda8e8c7f24e346e5cb',
-    user: 'internal1', // make default
-    number: '1-866-578-2331',
-  }
+    key: "JSbfbe6e3aef084885af8a574bec4f8d45",
+    user: "internal1", // make default
+    number: "1-866-790-0241",
+  },
+  //{
+    //key: 'JS8d271f1ea8034bda8e8c7f24e346e5cb',
+    //user: 'internal1', // make default
+    //number: '1-866-578-2331',
+  //}
 ];
 
 export function useRingbaUser(hist) {
